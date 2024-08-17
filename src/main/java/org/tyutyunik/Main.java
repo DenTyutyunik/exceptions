1617945d1dcfb467cf911845c5340b92b1012ffa
DenTyutyunik <den.tyutyunik@gmail.com>
2024-08-17 20:51:37 +0400

    homework was done

--- src/main/java/org/tyutyunik/Main.java
+++ src/main/java/org/tyutyunik/Main.java
@@ -1,7 +1,27 @@
 package org.tyutyunik;
 
+import org.tyutyunik.service.UserValidator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        // Input data
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите логин: ");
+        String login = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        String password = scanner.nextLine();
+        System.out.print("Повторите пароль: ");
+        String confirmPassword = scanner.nextLine();
+
+        // Check data
+        try {
+            UserValidator.validateUser(login, password, confirmPassword);
+        } catch (UserValidator.WrongLoginException | UserValidator.WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
